a7c6c0ad18e2aa87f95c5586418a4e920404b8e1
Oleksandr Timoshchuk <o.timoshuk@gmail.com>
2018-05-21 15:51:32 +0300

    Add 2 green-services__item

--- app/js/custom.js
+++ app/js/custom.js
@@ -1,1 +1,29 @@
 "use strict";
+
+window.onload = function () {
+  $(".green-services__slider").slick({
+    mobileFirst: true,
+    dots: true,
+    arrows: false,
+    autoplay: true,
+    speed: 300,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    responsive: [{
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3
+      }
+    }, {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2
+      }
+    }, {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1
+      }
+    }]
+  });
+};
---
+++ app/scripts/custom.js
@@ -0,0 +1,31 @@
+window.onload = function() {
+  $(".green-services__slider").slick({
+    mobileFirst: true,
+    dots: true,
+    arrows: false,
+    autoplay: true,
+    speed: 300,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1
+        }
+      }
+    ]
+  });
+};
